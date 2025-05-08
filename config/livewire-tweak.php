2e41709ddaeb6a30fcd200c4087c9d75f3fe36c2
Massimo <massimo@emarket.pe>
2025-05-08 23:41:26 +0200

    Update default configuration file

--- config/livewire-tweak.php
+++ config/livewire-tweak.php
@@ -18,6 +18,15 @@ return
             'domain' => env('LIVEWIRE_TWEAK_CORE_PREFIX_DOMAIN',true)
         ],
 
+        'custom' =>
+        [
+            'enable' => env('LIVEWIRE_TWEAK_CORE_CUSTOM_ENABLE',false),
+            'prefix' => env('LIVEWIRE_TWEAK_CORE_CUSTOM_PREFIX',''),
+            'assets' => env('LIVEWIRE_TWEAK_CORE_CUSTOM_ASSETS',''),
+            'routes' => env('LIVEWIRE_TWEAK_CORE_CUSTOM_ROUTES',''),
+            'domain' => env('LIVEWIRE_TWEAK_CORE_CUSTOM_DOMAIN',true)
+        ],
+
         'routes' =>
         [
             'remove' => env('LIVEWIRE_TWEAK_CORE_ROUTES_REMOVE',false)
@@ -38,6 +47,15 @@ return
             'assets' => env('LIVEWIRE_TWEAK_FLUX_PREFIX_ASSETS',''),
             'routes' => env('LIVEWIRE_TWEAK_FLUX_PREFIX_ROUTES',''),
             'domain' => env('LIVEWIRE_TWEAK_FLUX_PREFIX_DOMAIN',true)
+        ],
+
+        'custom' =>
+        [
+            'enable' => env('LIVEWIRE_TWEAK_FLUX_CUSTOM_ENABLE',false),
+            'prefix' => env('LIVEWIRE_TWEAK_FLUX_CUSTOM_PREFIX',''),
+            'assets' => env('LIVEWIRE_TWEAK_FLUX_CUSTOM_ASSETS',''),
+            'routes' => env('LIVEWIRE_TWEAK_FLUX_CUSTOM_ROUTES',''),
+            'domain' => env('LIVEWIRE_TWEAK_FLUX_CUSTOM_DOMAIN',true)
         ],
 
         'routes' =>
