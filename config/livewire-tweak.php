2fface9ab82b80a0df19c741f618beab0fa8a94e
Massimo <massimo@emarket.pe>
2025-05-12 04:44:52 +0200

    Update default configuration file

--- config/livewire-tweak.php
+++ config/livewire-tweak.php
@@ -16,8 +16,16 @@ return
             'groups' => env('LIVEWIRE_TWEAK_CORE_PREFIX_GROUPS',''),
             'assets' => env('LIVEWIRE_TWEAK_CORE_PREFIX_ASSETS','livewire'),
             'routes' => env('LIVEWIRE_TWEAK_CORE_PREFIX_ROUTES','livewire'),
-            'domain' => env('LIVEWIRE_TWEAK_CORE_PREFIX_DOMAIN',true),
-            'middle' => env('LIVEWIRE_TWEAK_CORE_PREFIX_MIDDLE','*')
+            'domain' => env('LIVEWIRE_TWEAK_CORE_PREFIX_DOMAIN',true)
+        ],
+
+        'middleware' =>
+        [
+            'enable' => env('LIVEWIRE_TWEAK_CORE_MIDDLEWARE_ENABLE',false),
+            'assign' => env('LIVEWIRE_TWEAK_CORE_MIDDLEWARE_ASSIGN',''),
+            'assets' => env('LIVEWIRE_TWEAK_CORE_MIDDLEWARE_ASSETS',false),
+            'routes' => env('LIVEWIRE_TWEAK_CORE_MIDDLEWARE_ROUTES',false),
+            'remove' => env('LIVEWIRE_TWEAK_CORE_MIDDLEWARE_REMOVE',false)
         ]
     ],
 
@@ -35,8 +43,16 @@ return
             'groups' => env('LIVEWIRE_TWEAK_FLUX_PREFIX_GROUPS',''),
             'assets' => env('LIVEWIRE_TWEAK_FLUX_PREFIX_ASSETS','flux'),
             'routes' => env('LIVEWIRE_TWEAK_FLUX_PREFIX_ROUTES','flux'),
-            'domain' => env('LIVEWIRE_TWEAK_FLUX_PREFIX_DOMAIN',true),
-            'middle' => env('LIVEWIRE_TWEAK_FLUX_PREFIX_MIDDLE','*')
+            'domain' => env('LIVEWIRE_TWEAK_FLUX_PREFIX_DOMAIN',true)
+        ],
+
+        'middleware' =>
+        [
+            'enable' => env('LIVEWIRE_TWEAK_CORE_MIDDLEWARE_ENABLE',false),
+            'assign' => env('LIVEWIRE_TWEAK_CORE_MIDDLEWARE_ASSIGN',''),
+            'assets' => env('LIVEWIRE_TWEAK_CORE_MIDDLEWARE_ASSETS',false),
+            'routes' => env('LIVEWIRE_TWEAK_CORE_MIDDLEWARE_ROUTES',false),
+            'remove' => env('LIVEWIRE_TWEAK_CORE_MIDDLEWARE_REMOVE',false)
         ]
     ]
 ];
