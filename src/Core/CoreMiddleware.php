31b57d4ee0e7675f3a601a48c6ed66cd5cbe71ca
Massimo <massimo@emarket.pe>
2025-05-12 05:02:17 +0200

    Feature to add middleware

---
+++ src/Core/CoreMiddleware.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Emkcloud\LivewireTweak\Core;
+
+class CoreMiddleware
+{
+    public const ENABLE = 'livewire-tweak.core.middleware.enable';
+
+    public const ASSIGN = 'livewire-tweak.core.middleware.assign';
+
+    public const ASSETS = 'livewire-tweak.core.middleware.assets';
+
+    public const ROUTES = 'livewire-tweak.core.middleware.routes';
+
+    public const REMOVE = 'livewire-tweak.core.middleware.remove';
+}
---
+++ src/Flux/FluxMiddleware.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Emkcloud\LivewireTweak\Core;
+
+class FluxMiddleware
+{
+    public const ENABLE = 'livewire-tweak.flux.middleware.enable';
+
+    public const ASSIGN = 'livewire-tweak.flux.middleware.assign';
+
+    public const ASSETS = 'livewire-tweak.flux.middleware.assets';
+
+    public const ROUTES = 'livewire-tweak.flux.middleware.routes';
+
+    public const REMOVE = 'livewire-tweak.flux.middleware.remove';
+}
